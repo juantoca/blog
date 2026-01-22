3cab698c0c01e3fbc47433243eddea14bcbbb5a6
Juan Toca <elan17.programacion@gmail.com>
2026-01-22 11:29:09 +0100

    Localized rss feeds

---
+++ src/pages/[lang]/rss.xml.ts
@@ -0,0 +1,36 @@
+import { SITE } from '@/consts'
+import rss from '@astrojs/rss'
+import type { APIContext, APIRoute } from 'astro'
+import { getAllPosts, getAllPostsAndSubpostsLocale } from '@/lib/data-utils'
+import { i18n } from "astro:config/client";
+import { toCodes } from "astro:i18n";
+
+
+export async function getStaticPaths() {
+
+  var pages = [] 
+
+  const locales: [] = toCodes(i18n!.locales)
+
+  for(var l in locales){
+    pages = pages.concat([{params: {lang: locales[l]}}])
+  }
+
+  return pages
+}
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const posts = await getAllPostsAndSubpostsLocale(params["lang"])
+  
+  return rss({
+    title: SITE.title,
+    description: SITE.description,
+    site: SITE.href,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      link: `/blog/${post.id}/`,
+    })),
+  })
+};
