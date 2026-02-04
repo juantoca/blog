cb66fac330efa045f8b6470906497ded3012c2f9
Juan Toca <elan17.programacion@gmail.com>
2026-02-05 00:12:14 +0100

    Created tooltip component

---
+++ src/components/ui/tooltip/Tooltip.tsx
@@ -0,0 +1,24 @@
+import { Tooltip as TooltipReact } from "radix-ui";
+import './tooltip.css'
+
+const Tooltip = (props) => {
+	return (
+		<TooltipReact.Provider>
+			<TooltipReact.Root delayDuration={0}>
+				<TooltipReact.Trigger asChild>
+					<div className="TooltipTrigger">
+						{props.trigger}
+					</div>
+				</TooltipReact.Trigger>
+				<TooltipReact.Portal>
+					<TooltipReact.Content className="TooltipContent" sideOffset={5}>
+						{props.content}
+					<TooltipReact.Arrow className="TooltipArrow" />
+					</TooltipReact.Content>
+				</TooltipReact.Portal>
+			</TooltipReact.Root>
+		</TooltipReact.Provider>
+	);
+};
+
+export default Tooltip;
